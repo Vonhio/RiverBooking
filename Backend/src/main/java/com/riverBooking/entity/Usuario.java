7b4a1ab09fe52761ddf14a2370159e981edd780f
Antonio Pérez <antpersos@gmail.com>
2025-05-08 12:56:18 +0200

    UsuarioEntity added

---
+++ Backend/src/main/java/com/riverBooking/entity/Usuario.java
@@ -0,0 +1,70 @@
+package com.riverBooking.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "usuario", unique = true, nullable = false)
+	private String usuario;
+	
+	@Column(name = "password", nullable = false)
+	private String password;
+	
+	@Column(name = "rol", nullable = false)
+	private String rol;
+
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String usuario, String password, String rol) {
+		this.id = id;
+		this.usuario = usuario;
+		this.password = password;
+		this.rol = rol;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
+	
+}
